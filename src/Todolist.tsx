9e48842d6b3863926b4d5b8acf47da9253988a99
GALw395 <galw395@mail.ru>
2024-04-04 15:03:50 +0300

    Sprint 01, week 1/5

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,26 +1,36 @@
 
-type PropsType = {
+type TaskType = {
+    id: number
     title: string
+    isDone: boolean
 }
 
-export const Todolist = (props: PropsType) => {
-    
+type PropsType = {
+    title: string
+    tasks: TaskType[]
+}
+
+export const Todolist = ({title, tasks}: PropsType) => {
+
   return (
     <div>
-      <h3>{props.title}</h3>
+      <h3>{title}</h3>
       <div>
         <input />
         <button>+</button>
       </div>
       <ul>
         <li>
-          <input type="checkbox" checked={true} /> <span>HTML&CSS</span>
+          <input type="checkbox" checked={tasks[0].isDone} />
+          <span>{tasks[0].title}</span>
         </li>
         <li>
-          <input type="checkbox" checked={true} /> <span>JS</span>
+          <input type="checkbox" checked={tasks[1].isDone} />
+          <span>{tasks[1].title}</span>
         </li>
         <li>
-          <input type="checkbox" checked={false} /> <span>React</span>
+          <input type="checkbox" checked={tasks[2].isDone} />
+          <span>{tasks[2].title}</span>
         </li>
       </ul>
       <div>
